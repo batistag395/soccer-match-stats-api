eab7a1a26f1ac4606877e2433bd64aca50cf631b
Gustavo <batistag395@gmail.com>
2024-06-11 00:25:06 -0300

    criando repositories das entidades match, stadium e team

---
+++ src/main/java/org/example/soccermatchstatsapi/model/repository/TeamRepository.java
@@ -0,0 +1,12 @@
+package org.example.soccermatchstatsapi.model.repository;
+
+import org.example.soccermatchstatsapi.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findByName(String name);
+    List<Team> findByState(String state);
+    List<Team> findByStatusActive(boolean active);
+}
